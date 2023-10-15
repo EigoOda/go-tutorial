13719d17a3294f02d94f5e03e09b3fe2f9055275
EigoOda <dubs11kt@gmail.com>
2023-10-15 13:05:57 +0900

    Call greetings module from hello module

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -2,7 +2,7 @@ package greetings
 
 import "fmt"
 
-func Hello(name stting) string {
+func Hello(name string) string {
   message := fmt.Sprintf("Hi, %v. Welcome!", name)
-  return massage
+  return message
 }
