8de7ff5f86f9a6672b2ac9a23fcdf4efefefa514
EigoOda <dubs11kt@gmail.com>
2023-10-15 13:58:50 +0900

    Return and handle an error

--- hello/hello.go
+++ hello/hello.go
@@ -1,12 +1,20 @@
 package main
 
 import (
-  "fmt"
+	"fmt"
+	"log"
 
-  "example.com/greetings"
+	"example.com/greetings"
 )
 
 func main() {
-  message := greetings.Hello("Gladys")
-  fmt.Println(message)
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	message, err := greetings.Hello("")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(message)
 }
